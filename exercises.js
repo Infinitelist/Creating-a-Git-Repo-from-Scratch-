d67e4ed8aef819623c01775f5143c11d9f67b3f6
Infinitelist <65507856+Infinitelist@users.noreply.github.com>
2024-02-02 18:31:12 -0500

    Updated code for exercises.js

--- exercises.js
+++ exercises.js
@@ -1,5 +1,29 @@
 /* Rutherford Labbe: 02-02-2024 */
 
+/* ------------------------- NOTES -------------------------- */
+
+// Fastest way to flatten an array full of nested arrays
+
+/* --------------- The flat() Method --------------- */
+/**
+ * let arrays = [[1, 2, 3], [4, 5], [6]];
+ * let flattenArray = arrays.flat();
+ * console.log(flattenArray);
+ * */
+/* --------------- The flat() Method --------------- */
+
+/* ------- The concat Method + spread operator ------ */
+/**
+ * let arrays = [[1, 2, 3], [4, 5], [6]];
+ * let flattenArray = [].concat(...arrays);
+ * console.log(flattenArray);
+ * */
+/* ------- The concat Method + spread operator ------ */
+
+
+/* ------------------------- NOTES -------------------------- */
+
+
 // 3-1. flattening(list)
 
 // An array containing arrays
@@ -7,7 +31,14 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // Your code here.
 
-// → [1, 2, 3, 4, 5, 6]
+// Using reduce to flatten the array using the reduce and concat method
+const flattenArray = arrays.reduce(function(accumulator, currentArray) {
+    return accumulator.concat(...currentArray);
+  }, []);
+
+// Print result on console
+console.log(flattenArray);
+
 
 
 /* ---------------------------- Section Divider ----------------------------- */
@@ -15,6 +46,17 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 // 3-2. loop(value, test, update, body)
 
 // Your code here.
+
+function loop(value, testFunction, updateFunction, bodyFunction) {
+
+    for(let i = value; i > 0; i--){
+        testFunction(value);
+        bodyFunction(value);
+        value = updateFunction(value);
+    }
+
+}
+
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
@@ -24,7 +66,7 @@ loop(3, n => n > 0, n => n - 1, console.log);
 
 /* ---------------------------- Section Divider ----------------------------- */
 
-function everyLoop(array, test)
+/*function everyLoop(array, test)
 {
   // Your code here.
 }
@@ -40,6 +82,6 @@ console.log(everyLoop([2, 4, 16], n => n < 10));
 // → false
 console.log(everyLoop([], n => n < 10));
 // → true
-
+*/
 
 /* ---------------------------- Section Divider ----------------------------- */
