c9c817b27854fb230a3d0e5c792b81160f575022
Infinitelist <65507856+Infinitelist@users.noreply.github.com>
2024-02-02 15:43:28 -0500

    Updated code for index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let randomInteger = Math.floor(Math.random() * 3) + 1;
 // Displays quote depending on the number generated
 function randomQuoteGenerator(num){
     if(num === 1){
-        console.log(" \"If you only knew the magnificence of the 3, 6 and 9, then you would have the key to the universe.\" " + "- Nikola Tesla");
+        console.log("Famous Quote -" + " \"If you only knew the magnificence of the 3, 6 and 9, then you would have the key to the universe.\" " + "- Nikola Tesla");
     }else if(num === 2){
-        console.log(" \"Our entire biological system, the brain, and the Earth itself, work on the same frequencies.\" " + "- Nikola Tesla");
+        console.log("Famous Quote -" + " \"Our entire biological system, the brain, and the Earth itself, work on the same frequencies.\" " + "- Nikola Tesla");
     }else{
-        console.log(" \"The gift of mental power comes from God, Divine Being, and if we concentrate our minds on that truth, we become in tune with this great power. My Mother had taught me to seek all truth in the Bible. \" " + "- Nikola Tesla");
+        console.log("Famous Quote -" + " \"The gift of mental power comes from God, Divine Being, and if we concentrate our minds on that truth, we become in tune with this great power. My Mother had taught me to seek all truth in the Bible. \" " + "- Nikola Tesla");
     }
 }
 
